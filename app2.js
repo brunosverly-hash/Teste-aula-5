cfb305f625dbb9b58e11fbc5b2f3bdb47ce9b7fa
Bruno Silva Verly <brunosverly@gmail.com>
2026-06-30 21:29:47 -0300

    condicional de aprovação

---
+++ app2.js
@@ -0,0 +1,10 @@
+let nota = 30;
+
+if (nota >= 75) {
+    console.log("Aprovado");
+}else if (nota >= 60 && nota < 74) {
+    console.log("Recuperação");
+}else {
+    console.log("Reprovado");
+}
+

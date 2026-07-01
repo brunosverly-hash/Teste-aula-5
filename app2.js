5d14c8482db1048835d3d127961563b4f8c193d6
Bruno Silva Verly <brunosverly@gmail.com>
2026-06-30 21:33:20 -0300

    resultado

--- app2.js
+++ app2.js
@@ -1,8 +1,8 @@
-let nota = 30;
+let nota = 74;
 
 if (nota >= 75) {
     console.log("Aprovado");
-}else if (nota >= 60 && nota < 74) {
+}else if (nota >= 60 && nota <= 74) {
     console.log("Recuperação");
 }else {
     console.log("Reprovado");
